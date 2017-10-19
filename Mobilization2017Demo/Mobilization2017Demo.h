64b94f25640e9d9e5c5cd2699b24f74f63d190db
Jakub Mazur <jakub@kettu.pl>
2017-10-19 18:03:28 +0200

    Initial Commit

---
+++ Mobilization2017Demo/Mobilization2017Demo.h
@@ -0,0 +1,19 @@
+//
+//  Mobilization2017Demo.h
+//  Mobilization2017Demo
+//
+//  Created by Jakub Mazur on 19/10/2017.
+//  Copyright © 2017 Jakub Mazur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mobilization2017Demo.
+FOUNDATION_EXPORT double Mobilization2017DemoVersionNumber;
+
+//! Project version string for Mobilization2017Demo.
+FOUNDATION_EXPORT const unsigned char Mobilization2017DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mobilization2017Demo/PublicHeader.h>
+
+
