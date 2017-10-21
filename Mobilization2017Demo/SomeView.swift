b4b540939aac47c82df1935a37bed5e3c98e7e99
Jakub Mazur <jakub.mazur@wingu.de>
2017-10-21 15:03:16 +0200

    demo code

--- Mobilization2017Demo/SomeView.swift
+++ Mobilization2017Demo/SomeView.swift
@@ -6,8 +6,40 @@
 //  Copyright © 2017 Jakub Mazur. All rights reserved.
 //
 
-import UIKit
+#if os(macOS)
+    import AppKit
+    public typealias View = NSView
+    public typealias Color = NSColor
+#elseif os(iOS)
+    import UIKit
+    public typealias View = UIView
+    public typealias Color = UIColor
+#endif
 
-class SomeView: UIView {
-
+class SomeView: View {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        drawBorder(layer)
+    }
+    
+    func drawBorder(_ layer: CALayer?) {
+        guard let myLayer = layer else {
+            //fallback code
+            return
+        }
+        myLayer.borderColor = Color.red.cgColor
+        myLayer.borderWidth = 10
+        myLayer.add(viewAnimation(), forKey: "backgroundColor")
+    }
+    
+    func viewAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation()
+        animation.keyPath = "backgroundColor"
+        animation.fromValue = Color.red.cgColor
+        animation.toValue = Color.blue.cgColor
+        animation.duration = 5
+        animation.repeatCount = .infinity
+        return animation
+    }
 }
